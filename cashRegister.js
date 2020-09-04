56695022fcc380b806218af2a47f0bc77352b32c
kwanele-hub <kwanelemkhwanazi688@gmail.com>
2020-09-04 10:30:19 +0000

    cash register  and palindrome

---
+++ cashRegister.js
@@ -0,0 +1,21 @@
+function checkPrice() {
+    document = document.getElementById("Price")
+if (validate == true) {
+         validate = true;
+    checkAvailability = false
+    }
+else checkPrice = checkPrice
+
+    function checkCash() {
+    document = document.getElementById("Cash")
+if (validate == true) {
+         validate = true;
+    checkAvailability = false
+}
+
+function checkCid() {
+    document = document.getElementById("Cid")
+if (validate == true) {
+         validate = true;
+    checkAvailability = false
+    }
---
+++ return.js
@@ -0,0 +1,32 @@
+//
+
+function check_palindrome(str_Palindrome)
+var str = string_enter.localLowerCase().replace(palindrome)
+
+var cCount = o;
+if (cString === ""){
+    return false
+    console.log("palindrome")
+}
+return false;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
